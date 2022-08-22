b10ecd74e3b04109222b5940397590c48be5cb1a
Olegtron <olgerdapex@gmail.com>
2022-08-22 12:00:27 +0300

    new spec

---
+++ cypress/e2e/telnyx_tests/register.cy.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+
+import MainPage from '../telnyx_pageobjects/MainPage'
+import SignUpPage from '../telnyx_pageobjects/SignUpPage'
+import LoginPage from '../telnyx_pageobjects/LoginPage'
+import AccountPage from '../telnyx_pageobjects/AccountPage'
+import ProductPage from '../telnyx_pageobjects/ProductPage'
+import ReleaseNotesPage from '../telnyx_pageobjects/ReleaseNotesPage'
+
+describe('telnyx.com test suite', () => {
+
+    afterEach(() => {
+        cy.clearCookies()
+        cy.clearLocalStorage()
+    })
+
+    const random = Math.random().toString(36).substring(4,14);
+    const mainPage = new MainPage()
+    const signUpPage = new SignUpPage()
+    const loginPage = new LoginPage()
+    const accountPage = new AccountPage()
+    const productPage = new ProductPage()
+    const releaseNotesPage = new ReleaseNotesPage()
+
+    it(
+        'Registration with valid credentials',
+        {
+          retries: {
+            runMode: 10,
+            openMode: 10,
+          },
+        },
+        () => {
+            cy.visit('https://telnyx.com/')
+            mainPage.closeButton() //close alert window
+            mainPage.signUpButton()
+
+            signUpPage.fillEmail(random+"@gmail.com")
+            signUpPage.fillName(random)
+            signUpPage.fillPass(random+"A288a@")
+            signUpPage.check()
+            signUpPage.createAccButton()
+    
+            cy.wait(10000)
+            cy.url().should('eq', 'https://telnyx.com/sign-up/verify-email/f')
+            signUpPage.emailSendNotif().should('have.text', "We've sent you an email to activate your account")
+        }
+      )
+
+})
